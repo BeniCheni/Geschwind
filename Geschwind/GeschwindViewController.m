713082bea103b79ae3a17057ff45e2670acbd761
Ben Chen <benjaminlchen@gmail.com>
2015-04-10 14:59:05 -0400

    Code Improvement

--- Geschwind/GeschwindViewController.m
+++ Geschwind/GeschwindViewController.m
@@ -12,6 +12,10 @@
 
 @property (nonatomic, strong) UIWebView *webview;
 @property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UIButton *backButton;
+@property (nonatomic, strong) UIButton *forwardButton;
+@property (nonatomic, strong) UIButton *stopButton;
+@property (nonatomic, strong) UIButton *reloadButton;
 
 @end
 
@@ -20,23 +24,72 @@
 #pragma mark - UIViewController
 
 - (void)loadView {
-    UIView *mainView = [UIView new];
+    [self loadViewComponents:self];
+}
 
-    self.webview = [UIWebView new];
-    self.webview.delegate = self;
+- (void) loadViewComponents:(GeschwindViewController *)appViewController {
+    appViewController.textField = [UITextField new];
+    appViewController.textField.keyboardType = UIKeyboardTypeURL;
+    appViewController.textField.returnKeyType = UIReturnKeyDone;
+    appViewController.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    appViewController.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    appViewController.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    appViewController.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    appViewController.textField.delegate = appViewController;
     
-    self.textField = [UITextField new];
-    self.textField.keyboardType = UIKeyboardTypeURL;
-    self.textField.returnKeyType = UIReturnKeyDone;
-    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
-    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
-    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
-    self.textField.delegate = self;
+    appViewController.backButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [appViewController.backButton setEnabled:NO];
+    appViewController.forwardButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [appViewController.forwardButton setEnabled:NO];
+    appViewController.stopButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [appViewController.stopButton setEnabled:NO];
+    appViewController.reloadButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [appViewController.reloadButton setEnabled:NO];
     
-    [mainView addSubview:self.webview];
-    [mainView addSubview:self.textField];
-    self.view = mainView;
+    [@[appViewController.backButton,
+       appViewController.forwardButton,
+       appViewController.stopButton,
+       appViewController.reloadButton] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+           UIButton *button = (UIButton *) obj;
+           [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+           
+           switch (idx) {
+               case 0:
+                   [button setTitle:NSLocalizedString(@"Back", @"Back command") forState:UIControlStateNormal];
+                   [button addTarget:appViewController.webview action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+                   break;
+               case 1:
+                   [button setTitle:NSLocalizedString(@"Forward", @"Back command") forState:UIControlStateNormal];
+                   [button addTarget:appViewController.webview action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+                   break;
+               case 2:
+                   [button setTitle:NSLocalizedString(@"Stop", @"Stop command") forState:UIControlStateNormal];
+                   [button addTarget:appViewController.webview action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
+                   break;
+               case 3:
+                   [button setTitle:NSLocalizedString(@"Refresh", @"Reload command") forState:UIControlStateNormal];
+                   [button addTarget:appViewController.webview action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+                   break;
+               default:
+                   break;
+           }
+      }];
+    
+    appViewController.webview = [UIWebView new];
+    appViewController.webview.delegate = appViewController;
+    UIView *mainView = [UIView new];
+    
+    [@[appViewController.webview,
+       appViewController.textField,
+       appViewController.backButton,
+       appViewController.forwardButton,
+       appViewController.stopButton,
+       appViewController.reloadButton]
+           enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+               [mainView addSubview:(UIView *) obj];
+     }];
+    
+    appViewController.view = mainView;
 }
 
 - (void)viewDidLoad {
@@ -53,10 +106,18 @@
     
     static const CGFloat itemHeight = 50;
     CGFloat width = CGRectGetWidth(self.view.bounds);
-    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight;
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight - itemHeight; // top 50, bottom 50 CGFloat units
+    CGFloat buttonWidth = CGRectGetWidth(self.view.bounds) / 4;
     
     self.textField.frame = CGRectMake(0, 0, width, itemHeight);
     self.webview.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+    
+    CGFloat currentButton = 0;
+    
+    for (UIButton *thisButton in @[self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        thisButton.frame = CGRectMake(currentButton, CGRectGetMaxY(self.webview.frame), buttonWidth, itemHeight);
+        currentButton += buttonWidth;
+    }
 }
 
 #pragma mark - UITextFieldDelegate
@@ -65,6 +126,7 @@
     [textField resignFirstResponder];
     
     NSString *URLString = textField.text;
+    
     NSURL *URL = [NSURL URLWithString:URLString];
     
     if (!URL.scheme) {
@@ -82,7 +144,17 @@
 #pragma mark - UIWebViewDelegate
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Error", @"Error")
+    if ([self.textField.text isEqualToString:@""]) {
+        [self popAlert:error errorMessage:NSLocalizedString(@"Empty URL", @"Empty URL")];
+    }
+    
+    [self popAlert:error errorMessage:NSLocalizedString(@"Error", @"Error")];
+}
+
+
+- (void)popAlert:(NSError *)error errorMessage:(NSString *)message {
+    // UIAlertView is deprecated in iOS8. Use UIAlertController instead
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:message
                                                                    message:[error localizedDescription]
                                                             preferredStyle:UIAlertControllerStyleAlert];
     
