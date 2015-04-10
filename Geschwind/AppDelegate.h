a48b7ce9eee2eabfaf301b2e0b398264cdb8b7ca
Ben Chen <benjaminlchen@gmail.com>
2015-04-09 22:35:02 -0400

    Initial Commit

---
+++ Geschwind/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Geschwind
+//
+//  Created by Beni Cheni on 4/9/15.
+//  Copyright (c) 2015 Princess of Darkness Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Geschwind/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Geschwind
+//
+//  Created by Beni Cheni on 4/9/15.
+//  Copyright (c) 2015 Princess of Darkness Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
